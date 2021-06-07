108d4cb4b45b584e29933ee04e570273711355cf
GET Academy <terje@kolderup.net>
2021-06-07 15:28:10 +0200

    x

--- api.php
+++ api.php
@@ -1,9 +1,9 @@
 <?php
     $filename = $_FILES['file']['name'];
-    $location = "uploads/".$filename;
+    $location = "uploads/".uniqid().$filename;
     $isSuccess = false;
     if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
         $isSuccess = true;
     } 
-    echo "{isSuccess: $isSuccess}"; 
+    echo "{\"isSuccess\": $isSuccess, \"fileName\": \"$location\"}"; 
 ?>
